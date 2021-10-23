5e614319cfb7f66fd92bc53efc811c54759afe1f
DylanDunham03 <dylanad2@illinois.edu>
2021-10-23 17:57:06 -0500

    Complete Board 20x20

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,108 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-// import recharts
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
-// Going to make up some data
-const data = [
-  {
-    name: 'Day0',
-    Rabbits: 3000,
-    Wolves: 400,
-    amt: 2400,
-  },
-  {
-    name: 'Day1',
-    Rabbits: 2500,
-    Wolves: 600,
-    amt: 2210,
-  },
-  {
-    name: 'Day2',
-    Rabbits: 2000,
-    Wolves: 1000,
-    amt: 2290,
-  },
-  {
-    name: 'Day3',
-    Rabbits: 1000,
-    Wolves: 700,
-    amt: 2000,
-  },
-  {
-    name: 'Day4',
-    Rabbits: 1200,
-    Wolves: 600,
-    amt: 2181,
-  },
-  {
-    name: 'Day5',
-    Rabbits: 2000,
-    Wolves: 500,
-    amt: 2500,
-  },
-  {
-    name: 'Day6',
-    Rabbits: 2100,
-    Wolves: 600,
-    amt: 2100,
-  },
-];
-
-
-// export 
-// export default function App() {
-//   return (
-    
-//   );
-// }
+import Board from './components/Board';
 
 
 function App() {
   return (
-    <><LineChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 10,
-        right: 0,
-        left: 0,
-        bottom: 10
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="Rabbits"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="Wolves" stroke="#82ca9d" />
-    </LineChart>
-    
-    <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div></>
-  );
+    <div id="app">
+      <Board/>
+    </div>
+  );  
 }
 
 export default App;
