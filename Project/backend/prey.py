7ece92f2a0625322fb7a53ec0ce08cbc7d49de03
Aakash Kumar <akumar2@imsa.edu>
2021-10-23 13:45:19 -0500

    loki

---
+++ Project/backend/prey.py
@@ -0,0 +1,34 @@
+from predator import predator
+from distance import usefulFunctions
+
+class prey:
+    def __intit__(self, species, population, startEnergy, energyToBreed, energyToRun, evadeVision, runSpeed, setPositionX, setPositionY, litterSize):
+        self.species = species
+        self.population = population
+        self.startEnergy = energy
+        self.energyToBreed = energyToBreed
+        self.energyToRun = energyToRun
+        self.vision = vision
+        self.runSpeed = runSpeed
+        self.setPositionX = setPositionX
+        self.setPositionY = setPositionY
+        
+    
+    #def move(speed, time):
+        #distance = speed * time
+
+    #def evade(vision):
+        #if(vision < o):
+            #vision = 0;
+    
+    #For now we will take two ints; later we will take this data from the website
+    #def setPosition(self, number1, number2): 
+        #self.setPositionX = number1
+        #self.setPositionY = number2
+    
+    #def populate(): 
+        #return 0;
+    
+
+
+        
