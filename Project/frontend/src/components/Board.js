d64587d84bf870aa8294a8704840ccdbab28428c
DylanDunham03 <dylanad2@illinois.edu>
2021-10-25 13:30:59 -0500

    Fixed bug, colors should correctly be displayed

--- Project/frontend/src/components/Board.js
+++ Project/frontend/src/components/Board.js
@@ -2,29 +2,73 @@ import React from "react";
 
 import './Board.css';
 
-const verticalaxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
-const horizontalaxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
+const verticalaxis = 20;
+const horizontalaxis = 20;
+// animals is a 2-d array, pass w values 0,1,2,3,4,5
+let animals = Array(verticalaxis).fill().map(() => Array(horizontalaxis).fill(0));
+animals[1][1] = 1
+animals[18][5] = 2
+animals[1][2] = 3
+animals[11][4] = 4
+animals[12][6] = 5
 
 export default function board() {
     let board = [];
+    for(let j = verticalaxis - 1; j >= 0; j--) {
+        for(let i = 0; i < horizontalaxis; i++) {
 
-    for(let j = verticalaxis.length - 1; j >= 0; j--) {
-        for(let i = 0; i < horizontalaxis.length; i++) {
-            const number = i + j + 2;
-            // choosing what we display on board here
-            if(number % 2 === 0) {
-                board.push(
-                    <div className = "tile grass-tile"></div>
-                );
-            } else {
+            if (animals[i][j] === 0) {
                 board.push(
                     <div className = "tile grass-tile"></div>
                 );
             }
+            // Grass only ^
 
+            if (animals[i][j] === 1) {
+                board.push(
+                    <div className = "tile grass-tile">
+                        <div className = "tile prey-tile">
+
+                        </div>
+                    </div>
+                );
+            } // Prey & grass ^
+            if (animals[i][j] === 2) {
+                board.push(
+                    <div className = "tile grass-tile">
+                        <div className = "tile predator-tile">
+
+                        </div>
+                    </div>
+                );
+            } 
+            //  Predator & grass ^
+            if (animals[i][j] === 3) {
+                board.push(
+                    <div className = "tile dirt-tile"></div>
+                );
+            }
+            // Dirt only ^
+            if (animals[i][j] === 4) {
+                board.push(
+                    <div className = "tile dirt-tile">
+                        <div className = "tile prey-tile">
+
+                        </div>
+                    </div>
+                );
+            }
+            // Prey and dirt ^
+            if (animals[i][j] === 5) {
+                board.push(
+                    <div className = "tile dirt-tile">
+                        <div className = "tile predator-tile">
+                        </div>
+                    </div>
+                );
+            }
+            // Predator and dirt ^
         }
     }
     return <div id= "board">{board}</div>;
 }
-
-// react components must be capital 
