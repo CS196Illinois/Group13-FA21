2e21715ff3b34ff41d6217605605348411e1214c
Aakash Kumar <akumar2@imsa.edu>
2021-11-13 22:37:50 -0600

    Fixed issues : updated backend

--- Project/backend/distance.py
+++
@@ -1,6 +0,0 @@
-import math
-class utilFunctions:
-    def distance(xCoordinate, yCoordinate):
-        distance = math.sqrt(math.pow(xCoordinate, 2) + math.pow(yCoordinate, 2));
-        return (distance);
-    
