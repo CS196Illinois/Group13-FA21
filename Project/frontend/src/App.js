dffe8666976fc786cc895734546cfd229ed71404
DylanDunham03 <dylanad2@illinois.edu>
2021-12-07 23:48:07 -0600

    n - 1 from final

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,6 +1,6 @@
 
    
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './App.css';
 import Board from './components/Board';
@@ -14,13 +14,92 @@ import Graph from './components/Graph';
 
 function App() {
 
-  const [initialData, setInitialData] = useState([{}])
 
-  useEffect(()=> {
-    fetch('/test').then(
-      response => response.json()
-    ).then(data => console.log(data))
+
+  const [initialData, setInitialData] = useState({
+      "0": [{
+            id: "thing",
+            type: "predator",
+            position: {
+              x: 0,
+              y: 0,
+            }
+            },
+           ],
+      "1": [{
+          id: "two",
+          type: "predator",
+          position: {
+            x: 15,
+            y: 11,
+            }
+            },
+           ],
+           "2": [{
+            id: "three",
+            type: "predator",
+            position: {
+              x: 7,
+              y: 21,
+              }
+              },
+             ]  
+
+              
+
+
+  })
+  const [Playing, setPlaying] = useState(false)
+  const [frame, setFrame] = useState(0)
+  const frameRef = useRef(0)
+  const [intervalID, setIntervalID] = useState()
+
+
+  useEffect(() => {
+
+    if (!Playing) {
+    const IntID = setInterval(() => {
+    console.log("frame time")
+    setFrame(frame + 1)}, 1000);
+    if (frame > Object.keys(initialData).length) {
+      setPlaying(!Playing);
+      clearInterval(IntID);
+    }
+    return () => clearInterval(IntID);
+    } 
+
   });
+
+  // useEffect(() => {
+  //   setFrame(frameRef.current)
+  //   console.log(frame)
+  // });
+
+  function handleStart() {
+    setPlaying(!Playing)
+    // if (!Playing) {
+    //   setIntervalID(setInterval(() => {
+    //     console.log("frame time")
+    //     console.log(frameRef)
+    //     frameRef.current++}, 1000));
+    // } else {
+    //   clearInterval(intervalID);
+    // }
+
+  }
+  
+  // setInitialData({
+  //   "0": [[1, (5,5)], [2, [5,7]]],
+  //   "1": [[1, (6,4)], [2, [6,8]]]
+
+  // })
+
+  // useEffect(()=> {
+  //   fetch('/test').then(
+  //     response => response.json()
+  //   ).then(data => setInitialData(data))
+  // },[]);
+
 
   // for prey
   const [rangeValue, setRangeValue] = useState(10);
@@ -40,8 +119,13 @@ function App() {
 
     <div id="app">
       <h1>{initialData.data}</h1>
-      <Board/>
-      <button class="button buttonstart">Start</button>
+      <Board
+          playbackData = {initialData}
+          start = {Playing}
+          frame = {frame}
+      
+      />
+      <button class="button buttonstart" onClick = {handleStart}>Start</button>
       <p>Prey Starting Population</p>      
       <SliderPrey
           min={0}
